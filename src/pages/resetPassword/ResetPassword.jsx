517df0c8a96bcc2a30f2ab4f6e6eb3614b416e24
Muhammad Syahdan Abdillah <125039086+msyahdana@users.noreply.github.com>
2023-06-04 21:50:01 +0700

    feature: resetPassword (#1)

--- src/pages/resetPassword/ResetPassword.jsx
+++ src/pages/resetPassword/ResetPassword.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
-import "./Login.css";
-import { Link } from "react-router-dom";
+import "./resetPassword.css";
 import logo from "../../assets/image/logo.png";
 
-const Login = () => {
+const ResetPassword = () => {
   return (
     <Container fluid className="vh-100">
       <Row className="h-100">
@@ -13,33 +12,22 @@ const Login = () => {
         </Col>
         <Col className="d-flex align-items-center justify-content-center ">
           <div>
-            <h3 className="fw-bold">Masuk</h3>
+            <h3 className="fw-bold">Reset Password</h3>
             <Form className="width-form mt-4">
               <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email/No Telepon</Form.Label>
-                <Form.Control type="email" placeholder="Contoh: johndoe@gmail.com" />
+                <Form.Label>Masukan password baru </Form.Label>
+                <Form.Control type="password" placeholder="Password baru" />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicPassword">
                 <div class="d-flex justify-content-between">
-                  <Form.Label>Password</Form.Label>
-                  <Link to="/reset-password" className="txt-color fw-bold">
-                    Lupa Kata Sandi
-                  </Link>
+                  <Form.Label>Ulangi password baru</Form.Label>
                 </div>
-
-                <Form.Control type="password" placeholder="Masukkan password" />
+                <Form.Control type="password" placeholder="Ulangi password" />
               </Form.Group>
               <Button type="submit" className="custom-button">
                 Submit
               </Button>
-              <div className="d-flex justify-content-center">
-                <Form.Text>
-                  <Link to="/register" className="txt-color">
-                    Daftar di sini
-                  </Link>
-                </Form.Text>
-              </div>
             </Form>
           </div>
         </Col>
@@ -48,4 +36,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default ResetPassword;
