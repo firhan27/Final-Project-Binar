4a367b385b01397f395fd8b8bf0d337118c923a8
msyahdana <msyahdanabdillah@gmail.com>
2023-06-04 21:49:11 +0700

    feature: resetPassword

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
 import Login from "./pages/login/Login";
+import ResetPassword from "./pages/resetPassword/ResetPassword";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
       </Routes>
     </BrowserRouter>
   );
