a05d073777e17d83f1612916764d43397d82994b
firhan27 <71623320+firhan27@users.noreply.github.com>
2023-06-07 19:40:05 +0700

    Homepage (#5)
    * Feature: Add Navbar, Banner, Pilih Tujuan, Destination Button
    
    * Feature: Add Destination Card

--- src/components/Form/FlightBookingForm.jsx
+++ src/components/Form/FlightBookingForm.jsx
@@ -1,94 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Row, Col, Button, Card, Container } from "react-bootstrap";
 import "./FlightBookingForm.css";
 
-class FlightBookingForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      from: "",
-      to: "",
-      departureDate: "",
-      returnDate: "",
-      passengers: 1,
-      seatClass: "economy",
-    };
-  }
+const FlightBookingForm = () => {
+  const [formData, setFormData] = useState({
+    from: "",
+    to: "",
+    departureDate: "",
+    returnDate: "",
+    passengers: 1,
+    seatClass: "economy",
+  });
 
-  handleInputChange = (event) => {
+  const handleInputChange = (event) => {
     const { name, value } = event.target;
-    this.setState({ [name]: value });
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
   };
 
-  handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    // Lakukan tindakan sesuai dengan data yang diisi
+    // Perform actions based on the filled data
   };
 
-  render() {
-    return (
-      <Container className="mt-4">
-        <Row className="justify-content-center">
-          <Col xs={12} md={6}>
-            <Card>
-              <Card.Body>
-                <Form onSubmit={this.handleSubmit}>
-                  <Form.Group controlId="from">
-                    <Form.Label>From</Form.Label>
-                    <Form.Control as="select" name="from" value={this.state.from} onChange={this.handleInputChange}>
-                      <option value="">Select Origin</option>
-                      {/* Daftar negara asal */}
-                    </Form.Control>
-                  </Form.Group>
+  return (
+    <Container className="mt-4">
+      <Row className="justify-content-center">
+        <Col xs={12} md={6}>
+          <Card>
+            <Card.Body>
+              <Form onSubmit={handleSubmit}>
+                <Form.Group controlId="from">
+                  <Form.Label>From</Form.Label>
+                  <Form.Control as="select" name="from" value={formData.from} onChange={handleInputChange}>
+                    <option value="">Select Origin</option>
+                    {/* List of origin countries */}
+                  </Form.Control>
+                </Form.Group>
 
-                  <Form.Group controlId="to">
-                    <Form.Label>To</Form.Label>
-                    <Form.Control as="select" name="to" value={this.state.to} onChange={this.handleInputChange}>
-                      <option value="">Select Destination</option>
-                      {/* Daftar negara tujuan */}
-                    </Form.Control>
-                  </Form.Group>
+                <Form.Group controlId="to">
+                  <Form.Label>To</Form.Label>
+                  <Form.Control as="select" name="to" value={formData.to} onChange={handleInputChange}>
+                    <option value="">Select Destination</option>
+                    {/* List of destination countries */}
+                  </Form.Control>
+                </Form.Group>
 
-                  <Form.Group controlId="departureDate">
-                    <Form.Label>Departure Date</Form.Label>
-                    <Form.Control type="date" name="departureDate" value={this.state.departureDate} onChange={this.handleInputChange} />
-                  </Form.Group>
+                <Form.Group controlId="departureDate">
+                  <Form.Label>Departure Date</Form.Label>
+                  <Form.Control type="date" name="departureDate" value={formData.departureDate} onChange={handleInputChange} />
+                </Form.Group>
 
-                  <Form.Group controlId="returnDate">
-                    <Form.Label>Return Date</Form.Label>
-                    <Form.Control type="date" name="returnDate" value={this.state.returnDate} onChange={this.handleInputChange} />
-                  </Form.Group>
+                <Form.Group controlId="returnDate">
+                  <Form.Label>Return Date</Form.Label>
+                  <Form.Control type="date" name="returnDate" value={formData.returnDate} onChange={handleInputChange} />
+                </Form.Group>
 
-                  <Row>
-                    <Col>
-                      <Form.Group controlId="passengers">
-                        <Form.Label>Passengers</Form.Label>
-                        <Form.Control type="number" name="passengers" min={1} value={this.state.passengers} onChange={this.handleInputChange} />
-                      </Form.Group>
-                    </Col>
-                    <Col>
-                      <Form.Group controlId="seatClass">
-                        <Form.Label>Seat Class</Form.Label>
-                        <Form.Control as="select" name="seatClass" value={this.state.seatClass} onChange={this.handleInputChange}>
-                          <option value="economy">Economy Class</option>
-                          <option value="business">Business Class</option>
-                        </Form.Control>
-                      </Form.Group>
-                    </Col>
-                  </Row>
-                  <Col className="p-2 text-center">
-                    <Button type="submit" className="custom-button mt-4 text-light">
-                      Cari Penerbangan
-                    </Button>
+                <Row>
+                  <Col>
+                    <Form.Group controlId="passengers">
+                      <Form.Label>Passengers</Form.Label>
+                      <Form.Control type="number" name="passengers" min={1} value={formData.passengers} onChange={handleInputChange} />
+                    </Form.Group>
                   </Col>
-                </Form>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
-}
+                  <Col>
+                    <Form.Group controlId="seatClass">
+                      <Form.Label>Seat Class</Form.Label>
+                      <Form.Control as="select" name="seatClass" value={formData.seatClass} onChange={handleInputChange}>
+                        <option value="economy">Economy Class</option>
+                        <option value="business">Business Class</option>
+                      </Form.Control>
+                    </Form.Group>
+                  </Col>
+                </Row>
+                <Col className="p-2 text-center">
+                  <Button type="submit" className="custom-button mt-4 text-light">
+                    Cari Penerbangan
+                  </Button>
+                </Col>
+              </Form>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default FlightBookingForm;
