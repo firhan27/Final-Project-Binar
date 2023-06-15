bdc48b8b729389c6114d65e3947be6f5a6b7d0ac
imran <muhammadalimran22@gmail.com>
2023-06-16 06:40:11 +0700

    checkout and payment

--- src/components/Form/FlightBookingForm.jsx
+++ src/components/Form/FlightBookingForm.jsx
@@ -34,7 +34,12 @@ const FlightBookingForm = () => {
               <Form onSubmit={handleSubmit}>
                 <Form.Group controlId="from">
                   <Form.Label>From</Form.Label>
-                  <Form.Control as="select" name="from" value={formData.from} onChange={handleInputChange}>
+                  <Form.Control
+                    as="select"
+                    name="from"
+                    value={formData.from}
+                    onChange={handleInputChange}
+                  >
                     <option value="">Select Origin</option>
                     {/* List of origin countries */}
                   </Form.Control>
@@ -42,32 +47,58 @@ const FlightBookingForm = () => {
 
                 <Form.Group controlId="to">
                   <Form.Label>To</Form.Label>
-                  <Form.Control as="select" name="to" value={formData.to} onChange={handleInputChange}>
+                  <Form.Control
+                    as="select"
+                    name="to"
+                    value={formData.to}
+                    onChange={handleInputChange}
+                  >
                     <option value="">Select Destination</option>
                     {/* List of destination countries */}
                   </Form.Control>
                 </Form.Group>
                 <Form.Group controlId="departureDate">
                   <Form.Label>Departure Date</Form.Label>
-                  <Form.Control type="date" name="departureDate" value={formData.departureDate} onChange={handleInputChange} />
+                  <Form.Control
+                    type="date"
+                    name="departureDate"
+                    value={formData.departureDate}
+                    onChange={handleInputChange}
+                  />
                 </Form.Group>
 
                 <Form.Group controlId="returnDate">
                   <Form.Label>Return Date</Form.Label>
-                  <Form.Control type="date" name="returnDate" value={formData.returnDate} onChange={handleInputChange} />
+                  <Form.Control
+                    type="date"
+                    name="returnDate"
+                    value={formData.returnDate}
+                    onChange={handleInputChange}
+                  />
                 </Form.Group>
 
                 <Row>
                   <Col>
                     <Form.Group controlId="passengers">
                       <Form.Label>Passengers</Form.Label>
-                      <Form.Control type="number" name="passengers" min={1} value={formData.passengers} onChange={handleInputChange} />
+                      <Form.Control
+                        type="number"
+                        name="passengers"
+                        min={1}
+                        value={formData.passengers}
+                        onChange={handleInputChange}
+                      />
                     </Form.Group>
                   </Col>
                   <Col>
                     <Form.Group controlId="seatClass">
                       <Form.Label>Seat Class</Form.Label>
-                      <Form.Control as="select" name="seatClass" value={formData.seatClass} onChange={handleInputChange}>
+                      <Form.Control
+                        as="select"
+                        name="seatClass"
+                        value={formData.seatClass}
+                        onChange={handleInputChange}
+                      >
                         <option value="economy">Economy Class</option>
                         <option value="business">Business Class</option>
                       </Form.Control>
@@ -75,7 +106,10 @@ const FlightBookingForm = () => {
                   </Col>
                 </Row>
                 <Col className="p-2 text-center">
-                  <Button type="submit" className="custom-button mt-4 text-light w-75">
+                  <Button
+                    type="submit"
+                    className="custom-button mt-4 text-light w-75"
+                  >
                     Cari Penerbangan
                   </Button>
                 </Col>
