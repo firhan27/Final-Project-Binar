52f0b50cabbfd66f360aac48c998f7cf5ddf634a
firhan27 <71623320+firhan27@users.noreply.github.com>
2023-06-22 17:38:06 +0700

    Homepage (#23)
    * Feature: Add Navbar, Banner, Pilih Tujuan, Destination Button
    
    * Feature: Add Destination Card
    
    * feature: Add icons, finishing component
    
    * Feature: Fixing Responsive
    
    * Feature: Fixing Slice Homepage
    
    * Feature: Consume API

--- src/components/Header/NavbarComponent.jsx
+++ src/components/Header/NavbarComponent.jsx
@@ -1,17 +1,9 @@
 import React from "react";
-import {
-  Button,
-  Container,
-  FormControl,
-  InputGroup,
-  Nav,
-  Navbar,
-} from "react-bootstrap";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 import logo from "../../assets/image/logo.png";
 import fontLogin from "../../assets/image/fontLogin.png";
-import { FaSearch } from "react-icons/fa";
 
 const NavbarComponent = ({ type }) => {
   return (
@@ -20,25 +12,10 @@ const NavbarComponent = ({ type }) => {
         <Nav className="mr-auto mb-3 align-items-center">
           <Navbar.Brand as={Link} to="/" className="text-dark">
             <div>
-              <img
-                src={logo}
-                alt="logo"
-                fluid
-                style={{ width: "70px", height: "50px" }}
-              />
+              <img src={logo} alt="logo" fluid style={{ width: "70px", height: "50px" }} />
             </div>
           </Navbar.Brand>
           <div style={{ width: "20px" }}></div>
-          <InputGroup className="">
-            <FormControl
-              type="text"
-              placeholder="Cari..."
-              className="mr-sm-2 search-input"
-            />
-            <InputGroup.Text className="search-input">
-              <FaSearch style={{ fontSize: "14px" }} />
-            </InputGroup.Text>
-          </InputGroup>
         </Nav>
         <Nav className="me mr-auto">
           <Button className="button-color text-light" as={Link} to={"/login"}>
