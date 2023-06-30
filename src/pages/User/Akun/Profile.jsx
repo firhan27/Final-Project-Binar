c6b434997c9844a544b6c25f58c3e1c2a772390f
Muhammad Syahdan Abdillah <125039086+msyahdana@users.noreply.github.com>
2023-06-30 20:07:49 +0700

    remove consolelog (#39)

--- src/pages/User/Akun/Profile.jsx
+++ src/pages/User/Akun/Profile.jsx
@@ -1,14 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {
-  Container,
-  Row,
-  Col,
-  Stack,
-  Button,
-  Card,
-  Modal,
-  Form,
-} from "react-bootstrap";
+import { Container, Row, Col, Stack, Button, Card, Modal, Form } from "react-bootstrap";
 import { IoArrowBack } from "react-icons/io5";
 import NavbarComponent from "../../../components/Header/NavbarComponent";
 import { Link } from "react-router-dom";
@@ -25,14 +16,11 @@ const Profile = () => {
       try {
         const token = localStorage.getItem("token");
 
-        const response = await axios.get(
-          `https://skypass-dev.up.railway.app/user/whoami`,
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
+        const response = await axios.get(`https://skypass-dev.up.railway.app/user/whoami`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
 
         const data = response.data.data.user;
         setDataUser(data);
@@ -111,7 +99,7 @@ const Profile = () => {
                 <p>Nomor Telepon: {dataUser?.phone}</p>
                 <p>Email: {dataUser?.email}</p>
                 <Button variant="light" onClick={openModal}>
-                  Ubah Nama
+                  Ubah Profile
                 </Button>
               </Card.Body>
             </Card>
@@ -126,11 +114,7 @@ const Profile = () => {
           <Form>
             <Form.Group controlId="formNewName">
               <Form.Label>Nama Baru</Form.Label>
-              <Form.Control
-                type="text"
-                value={newName}
-                onChange={(e) => setNewName(e.target.value)}
-              />
+              <Form.Control type="text" value={newName} onChange={(e) => setNewName(e.target.value)} />
             </Form.Group>
           </Form>
         </Modal.Body>
