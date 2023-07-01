0503f7428479cf4084e4b992b701115118e3f1a6
firhan27 <71623320+firhan27@users.noreply.github.com>
2023-07-01 13:54:44 +0700

    Homepage (#41)
    * Feature: Add Navbar, Banner, Pilih Tujuan, Destination Button
    
    * Feature: Add Destination Card
    
    * feature: Add icons, finishing component
    
    * Feature: Fixing Responsive
    
    * Feature: Fixing Slice Homepage
    
    * Feature: Consume API
    
    * feature: add icon
    
    * Feature: Fix Code
    
    * Feature: Add Data
    
    * feature: add api
    
    * feature: fix code
    
    * feature: fix itemCardTicket, add icon

--- src/components/ItemCardTicket/ItemCardTicket.jsx
+++ src/components/ItemCardTicket/ItemCardTicket.jsx
@@ -3,14 +3,30 @@ import DetailCardTicket from "../DetailCardTicket/DetailCardTicket";
 import "./ItemCardTicket.css";
 import { BsChevronDown, BsChevronUp } from "react-icons/bs";
 import { Link } from "react-router-dom";
-import { Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import { Button } from "react-bootstrap";
 
 const ItemCardTicket = ({ data, isActive, onClick }) => {
   const navigate = useNavigate();
   const handleSelectTicket = () => {
     navigate("/checkout", { state: { id: data.id } });
   };
+
+  const formatCurrency = (amount) => {
+    const formattedAmount = new Intl.NumberFormat("id-ID", {
+      style: "currency",
+      currency: "IDR",
+      minimumFractionDigits: 0, // Mengatur jumlah digit desimal minimum
+      maximumFractionDigits: 0,
+    }).format(amount);
+
+    // Menghapus angka desimal setelah koma
+    const formattedPrice = formattedAmount.replace(/\.00$/, "");
+
+    return formattedPrice;
+  };
+
+  const formattedPrice = formatCurrency(data.price);
 
   return (
     <div className="card p-4 d-flex flex-column gap-1 border-0 shadow">
@@ -20,11 +36,7 @@ const ItemCardTicket = ({ data, isActive, onClick }) => {
           {data.airline_name} - {data.class}
         </p>
         <button className="bg-transparent border-0" onClick={onClick}>
-          {isActive !== null && isActive ? (
-            <BsChevronUp size={20} />
-          ) : (
-            <BsChevronDown size={20} />
-          )}
+          {isActive !== null && isActive ? <BsChevronUp size={20} /> : <BsChevronDown size={20} />}
         </button>
       </div>
       <div className="d-flex justify-content-between">
@@ -47,20 +59,13 @@ const ItemCardTicket = ({ data, isActive, onClick }) => {
           <i className="far fs-5 fa-suitcase-rolling text-purple"></i>
         </button>
         <div className="col-3 d-flex flex-column justify-content-end">
-          <p className="h5 fw-bolder text-purple text-center">
-            IDR {data.price}
-          </p>
-          <Button
-            className="btn btn-purple text-white w-75 mx-auto rounded-pill btn-ticket border-0"
-            as={Link}
-            to="/checkout"
-            onClick={handleSelectTicket}
-          >
+          <p className="h5 fw-bolder text-purple ms-4">{formattedPrice}</p>
+          <Button className="btn btn-purple text-white w-75 mx-auto rounded-pill btn-ticket" as={Link} to="/checkout" onClick={handleSelectTicket}>
             Pilih
           </Button>
         </div>
       </div>
-      {isActive !== null && isActive && <DetailCardTicket />}
+      {isActive !== null && isActive && <DetailCardTicket data={data} />}
     </div>
   );
 };
